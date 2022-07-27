993932ad71b2703dff8c9ee171ea36a85870b683
EricssonDasilva-code <dasilva.ericsson@gmail.com>
2022-07-26 23:09:08 -0300

    project Created

---
+++ src/test/java/com/ericsson/apiprodutos/ApiProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ericsson.apiprodutos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
