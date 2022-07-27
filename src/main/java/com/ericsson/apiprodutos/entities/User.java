37f2dac7f9b294a0b3ee94828f5fa486a1305ece
EricssonDasilva-code <dasilva.ericsson@gmail.com>
2022-07-27 00:09:32 -0300

    database seeding

--- src/main/java/com/ericsson/apiprodutos/entities/User.java
+++ src/main/java/com/ericsson/apiprodutos/entities/User.java
@@ -1,18 +1,16 @@
 package com.ericsson.apiprodutos.entities;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Objects;
 
 @Entity
+@Table(name = "TB_USER")
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
