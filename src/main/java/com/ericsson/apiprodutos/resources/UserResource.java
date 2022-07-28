04ea616d29900d7fd35d17fa0f88c2a77a926aa8
EricssonDasilva-code <dasilva.ericsson@gmail.com>
2022-07-27 22:46:59 -0300

    Service layer, component registration

--- src/main/java/com/ericsson/apiprodutos/resources/UserResource.java
+++ src/main/java/com/ericsson/apiprodutos/resources/UserResource.java
@@ -2,18 +2,33 @@ package com.ericsson.apiprodutos.resources;
 
 
 import com.ericsson.apiprodutos.entities.User;
+import com.ericsson.apiprodutos.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping
-    public ResponseEntity<User> findAll() {
-        User u = new User(1L, "ericsson", "dasilva.ericsson@gmail.com", "51998268579", "12345");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findAll() {
+
+        List<User> list = service.findAll();
+
+        return ResponseEntity.ok().body(list);
+    }
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findByID(@PathVariable Long id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
     }
 }
