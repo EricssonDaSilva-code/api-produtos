2efe680b0d556e006d5a5f1ac0e0ead4e807c7fd
EricssonDasilva-code <dasilva.ericsson@gmail.com>
2022-07-28 23:38:39 -0300

    Objects

--- src/main/java/com/ericsson/apiprodutos/config/TestConfig.java
+++ src/main/java/com/ericsson/apiprodutos/config/TestConfig.java
@@ -1,7 +1,9 @@
 package com.ericsson.apiprodutos.config;
 
+import com.ericsson.apiprodutos.entities.Product;
 import com.ericsson.apiprodutos.repositories.CategoryRepository;
 import com.ericsson.apiprodutos.repositories.OrderRepository;
+import com.ericsson.apiprodutos.repositories.ProductRepository;
 import com.ericsson.apiprodutos.repositories.UserRepository;
 import com.ericsson.apiprodutos.entities.Category;
 import com.ericsson.apiprodutos.entities.Order;
@@ -27,6 +29,9 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private CategoryRepository categoryRepository;
 
+    @Autowired
+    private ProductRepository productRepository;
+
     @Override
     public void run(String... args) throws Exception {
 
@@ -34,7 +39,15 @@ public class TestConfig implements CommandLineRunner {
         Category cat2 = new Category(null, "Books");
         Category cat3 = new Category(null, "Computers");
 
+        Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+        Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+        Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+        Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+        Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
+
+
         categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+        productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
--- src/main/java/com/ericsson/apiprodutos/entities/Category.java
+++ src/main/java/com/ericsson/apiprodutos/entities/Category.java
@@ -2,7 +2,9 @@ package com.ericsson.apiprodutos.entities;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name = "TB_CATEGORY")
@@ -13,6 +15,8 @@ public class Category implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
+    @Transient
+    private Set<Product> products = new HashSet<>();
 
     public Category() {
     }
@@ -40,6 +44,10 @@ public class Category implements Serializable {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Set<Product> getProducts() {
+        return products;
     }
 
     @Override
