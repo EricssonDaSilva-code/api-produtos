7b773a2b4d715d621299a9a61c42b74dbee8f0da
EricssonDasilva-code <dasilva.ericsson@gmail.com>
2022-07-30 23:14:20 -0300

    Exception handling - findById

--- src/main/java/com/ericsson/apiprodutos/services/UserService.java
+++ src/main/java/com/ericsson/apiprodutos/services/UserService.java
@@ -2,6 +2,7 @@ package com.ericsson.apiprodutos.services;
 
 import com.ericsson.apiprodutos.repositories.UserRepository;
 import com.ericsson.apiprodutos.entities.User;
+import com.ericsson.apiprodutos.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,7 +20,7 @@ public class UserService {
     }
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
